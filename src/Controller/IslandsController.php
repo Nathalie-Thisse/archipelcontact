483e40d672253545dc38a3bc896605a97a279e25
Léa Buguinet <lea.buguinet@gmail.com>
2021-06-03 11:28:49 +0200

    Bali + Tour1 page creation

--- src/Controller/IslandsController.php
+++ src/Controller/IslandsController.php
@@ -8,13 +8,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IslandsController extends AbstractController
 {
-    /**
-     * @Route("/islands", name="islands")
-     */
+  
+    #[Route('/islands', name: 'islands')]
     public function index(): Response
     {
         return $this->render('islands/index.html.twig', [
             'controller_name' => 'IslandsController',
         ]);
     }
+
+    #[Route('/islands/bali', name: 'bali')]
+    public function bali(): Response
+    {
+        return $this->render('islands/bali.html.twig', [
+            'controller_name' => 'IslandsController',
+        ]);
+    }
+
+   
 }
