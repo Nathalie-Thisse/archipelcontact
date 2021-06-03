9abfb9fc0ed9b6e19b99ccc2203186ca083d71b7
Nathalie <nathaliethisse@gmail.com>
2021-06-03 17:00:55 +0200

    Display tours on Tours page

--- src/Controller/ToursController.php
+++ src/Controller/ToursController.php
@@ -2,19 +2,50 @@
 
 namespace App\Controller;
 
+use App\Entity\Tours;
+use App\Entity\Days;
+use App\Repository\ToursRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ToursController extends AbstractController
 {
-    /**
-     * @Route("/tours", name="tours")
-     */
-    public function index(): Response
+
+    // Display all tours in Tours Page: 
+
+    #[Route('/tours', name: 'tours', methods: ['GET'])]
+    public function index(ToursRepository $toursRepository): Response
     {
         return $this->render('tours/index.html.twig', [
-            'controller_name' => 'ToursController',
+            'tours' => $toursRepository->findAll(),
+        ]);
+    }
+
+    // // 2d method: 
+    // /**
+    //  * @Route("/tours", name="tours")
+    //  */
+    // public function index(EntityManagerInterface $entityManager): Response
+    // {
+    //     $tours = $entityManager->getRepository(Tours::class)->findAll();
+
+    //     return $this->render('tours/index.html.twig', [
+    //         'tours' => $tours,
+    //     ]);
+    // }
+    
+ 	/**
+     * @Route("/tours/{id}", name="show_tour")
+    */
+    public function showTour($id, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $tour = $entityManager->getRepository(Tours::class)->find($id);
+
+        return $this->render('tours\tour.html.twig', [
+            'tour' => $tour,
         ]);
     }
 }
