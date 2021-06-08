94693b4dd00746338105d1b243e33ba8ffb09b61
Nathalie <nathaliethisse@gmail.com>
2021-06-08 10:43:47 +0200

    Display tours on bali page

--- src/Controller/IslandsController.php
+++ src/Controller/IslandsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Tours;
+use App\Repository\ToursRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IslandsController extends AbstractController
 {
   
-    #[Route('/islands', name: 'islands')]
+    #[Route('/islands', name: 'islands', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('islands/index.html.twig', [
@@ -18,10 +20,11 @@ class IslandsController extends AbstractController
     }
 
     #[Route('/islands/bali', name: 'bali')]
-    public function bali(): Response
+    public function bali(ToursRepository $toursRepository): Response
     {
         return $this->render('islands/bali.html.twig', [
-            'controller_name' => 'IslandsController',
+            'controller_name' => 'IslandsController',                        
+            'tours' => $toursRepository->findAll(),
         ]);
     }
 
