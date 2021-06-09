fb9f2425cd2c6fa63ed773555047a21e7d341d5c
Nathalie <nathaliethisse@gmail.com>
2021-06-09 16:36:36 +0200

    Js tab starting code implemented on Bali Page - test

--- assets/script.js
+++ assets/script.js
@@ -1,51 +1,27 @@
+// tabs
 
-// fonctions Toggle for hide/show activities information on Island Pages
-function onClick() {
-	// Récupération de l'élément .tab parent
-    const tab = this.closest('.tab');
-	// Récupération de l'élément "frère" suivant (ici .content)
-	const content = tab.nextElementSibling;
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
 
-	const contentDisplay = document.querySelector('.content.display');
-	if (contentDisplay && content != contentDisplay) {
-		toggle(contentDisplay);
-	}
 
-	// Ajout/Suppression de la class display
-	toggle(content);
-}
-
-function toggle(element)
-{
-	element.classList.toggle('display');
-}
-
-const btns = document.querySelectorAll('.tab .btn');
-btns.forEach(btn => {
-	btn.addEventListener('click', onClick);
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
 });
 
 
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var country = btnTarget.dataset.country;
 
-/*
-function onClick()
-{
-	document.querySelectorAll('.content.display').forEach(item => {
-		toggleClassDisplay(item);
-	})
-	
-	toggleClassDisplay(this.closest('.tab').nextElementSibling);
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + country).classList.add("active");
+   
+   btnTarget.classList.add("active");
 }
-
-
-function toggleClassDisplay(node)
-{
-	node.classList.toggle('display');
-}
-
-
-const btns = document.querySelectorAll('.tab .btn');
-btns.forEach(btn => {
-	btn.addEventListener('click', onClick);
-})
-*/
