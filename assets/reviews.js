73db436fa43a461847094d8812d86053d6a5bf3a
Nathalie <nathaliethisse@gmail.com>
2021-06-17 11:52:13 +0200

    New reviews carousel on Home page

--- assets/reviews.js
+++ assets/reviews.js
@@ -1,92 +1,88 @@
+/* ========== SLIDER LOGIC ==========
+1. Create selectors for the slider container, each slide, the next button and the previous button
+2. Clone the first slide and append it at the end of the slides container
+3. Clone the last slide and append it at the beginning of the slides container
 
-// ------------------------------- sidebar for INFORMATION PAGE
+NOTE: steps 2 and 3 will allow us to create an illusion of infinitely looping through the slider
 
-window.addEventListener('DOMContentLoaded',() => {
+4. Create a 'counter' variable and set it equal to 1
+5. Add event listeners to the next and previous buttons
+    If next button is clicked, add 1 to counter
+    If prev button is clicked, subtract 1 from counter
+6. Move to the next / previous slide by using transform: translateX to shift the slides horizontally
+by each slide's width and margin multiplied by the number of slides tracked in counter
 
-	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	});
+7. If the "last clone" is reached, reset counter to 0
+8. If the "first clone" is reached, reset counter to 1
+===================================== */
 
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
-	});
-	
+// SELECTORS
+const container = document.querySelector('.slider');
+const slider = document.querySelector('.slider__slides');
+const slides = document.querySelectorAll('.slider__slide');
+const prevBtn = document.querySelector('.slider__button--prev');
+const nextBtn = document.querySelector('.slider__button--next');
+const numSlides = slides.length;
 
+// DETERMINE THE LENGTH REQUIRED TO MOVE HORIZONTALLY TO THE NEXT SLIDE
+let slideWidth = slides[0].offsetWidth;
+let slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+let moveX = slideWidth + slideMarginRight;
 
+// CLONE FIRST AND LAST SLIDES AND ADD TO SLIDER
+const firstClone = slides[0].cloneNode(true);
+const lastClone = slides[numSlides - 1].cloneNode(true);
 
+firstClone.setAttribute('id', 'first-clone');
+lastClone.setAttribute('id', 'last-clone');
 
+slider.appendChild(firstClone);
+slider.prepend(lastClone);
 
-	// ------------------------------- reviews carousel on HOME PAGE
-	carousel();
-	function carousel(){
-		'use strict';
-		var slides = document.querySelectorAll('.testimonial-item'),
-			 button = document.getElementById('button'),
-			 arrows = document.querySelectorAll('.lnr'),
-			 carouselCount = 0,
-			 scrollInterval,
-			 interval = 5000;
-	
-		arrows[0].addEventListener('click', function (e) {
-			e = e || window.event;
-			e.preventDefault();
-			carouselCount -= 100;
-			slider();
-			if (e.type !== 'autoClick') {
-				clearInterval(scrollInterval);
-				scrollInterval = setInterval(autoScroll, interval);
-			}
-		});
-		arrows[1].addEventListener('click', sliderEvent);
-		arrows[1].addEventListener('autoClick', sliderEvent);
-		
-		function sliderEvent(e) {
-			e = e || window.event;
-			e.preventDefault();
-			carouselCount += 100;
-			slider();
-			if (e.type !== "autoClick") {
-				clearInterval(scrollInterval);
-				scrollInterval = setInterval(autoScroll, interval);
-			}
-		}
-		
-		function slider() {
-			switch (carouselCount) {
-				case -100:
-					carouselCount = 0;
-					break;
-				case 300:
-					carouselCount = 0;
-					break;
-				default:
-					break;
-			}
-			console.log(carouselCount);
-			for (var i = 0; i < slides.length; i += 1) {
-				slides[i].setAttribute('style', 'transform:translateX(-' + carouselCount + '%)');
-			}
-		}
-		
-		// create new Event to dispatch click for auto scroll
-		var autoClick = new Event('autoClick');
-		function autoScroll() {
-			arrows[1].dispatchEvent(autoClick);
-		}
-		
-		// set timing of dispatch click events
-		scrollInterval = setInterval(autoScroll, interval);
-		
-	};
+const allSlides = document.querySelectorAll('.slider__slide'); //allSlides includes the clones
+
+// SHOW THE FIRST SLIDE
+slider.style.transform = `translateX(${-moveX}px)`;
+
+// SET COUNTER AND ADD EVENT LISTENERS TO BUTTON;
+// EACH BUTTON CLICK SHOULD MOVE ALL SLIDES HORIZONTALLY BY MOVEX AMOUNT COUNTER TIMES
+let counter = 1;
+
+nextBtn.addEventListener('click', () => {
+    counter++;
+    slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    if (counter >= allSlides.length) counter = numSlides;
+    console.log(counter);
 });
 
+prevBtn.addEventListener('click', () => {
+    counter--;
+    slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    if (counter < 0) counter = 0
+});
 
+// CHECK FOR FIRSTCLONE AND LASTCLONE AND RESET THE SLIDES ACCORDINGLY
+slider.addEventListener('transitionend', () => {
+    if (allSlides[counter].id === 'first-clone') {
+        slider.style.transition = 'none';
+        counter = 1;
+        slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    }
 
+    if (allSlides[counter].id === 'last-clone') {
+        slider.style.transition = 'none';
+        counter = numSlides;
+        slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+    }
+});
+
+// ADJUST THE SLIDER POSITION WHEN THE SLIDER/VIEWPORT IS RESIZED
+window.addEventListener('resize', () => {
+    slideWidth = slides[0].offsetWidth;
+    slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+    moveX = slideWidth + slideMarginRight;
+    slider.style.transition = 'none';
+    slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+});
