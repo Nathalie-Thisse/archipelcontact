2e3ab99a8362ddd4cbbe2508b9f65c4128396bf1
Nathalie <nathaliethisse@gmail.com>
2021-07-05 13:25:10 +0200

    New photo carousel on Bali Page + cards for tours

--- assets/island.js
+++ assets/island.js
@@ -1,24 +1,2 @@
 
-// import Splide for carousel photos, the installation has been done with npm install @splidejs/splide 
-// more info: https://splidejs.com/
-
-import Splide from '@splidejs/splide';
-
-// function for photo carousel on ISLAND page
-
-	document.addEventListener( 'DOMContentLoaded', function () {
-
-		new Splide( '#image-slider', {
-			'cover'      : true,
-			'heightRatio': 0.5,
-			'height' : '200px',
-			'width' : '100%',
-			perPage: 4,
-			perMove: 1,
-			'gap' : '1rem',
-			autoplay : 'true',
-			type   : 'loop',
-		} ).mount();
-
-	} ); 
-	
+// JS for each island page (Bali, Java, etc.)
