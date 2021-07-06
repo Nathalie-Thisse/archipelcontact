276692fdd87f0c1587c46fa22733102be135e419
Léa Buguinet <lea.buguinet@gmail.com>
2021-07-06 17:22:06 +0200

    Homepage reviews carousel hover fix

--- assets/homepage.js
+++ assets/homepage.js
@@ -41,75 +41,74 @@ import Splide from '@splidejs/splide';
 	// ===================================== */
 
 	// 	// SELECTORS
-	// 		const container = document.querySelector('.slider');
-	// 		const slider = document.querySelector('.slider__slides');
-	// 		const slides = document.querySelectorAll('.slider__slide');
-	// 		const prevBtn = document.querySelector('.slider__button--prev');
-	// 		const nextBtn = document.querySelector('.slider__button--next');
-	// 		const numSlides = slides.length;
+	 		const container = document.querySelector('.slider');
+	 		const slider = document.querySelector('.slider__slides');
+	 		const slides = document.querySelectorAll('.slider__slide');
+	 		const prevBtn = document.querySelector('.slider__button--prev');
+	 		const nextBtn = document.querySelector('.slider__button--next');
+	 		const numSlides = slides.length;
 
 	// //     // DETERMINE THE LENGTH REQUIRED TO MOVE HORIZONTALLY TO THE NEXT SLIDE
-	// 		let slideWidth = slides[0].offsetWidth;
-	// 		let slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
-	// 		let moveX = slideWidth + slideMarginRight;
+	 		let slideWidth = slides[0].offsetWidth;
+	 		let slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+	 		let moveX = slideWidth + slideMarginRight;
 
 	// //     // CLONE FIRST AND LAST SLIDES AND ADD TO SLIDER
-	// 		const firstClone = slides[0].cloneNode(true);
-	// 		const lastClone = slides[numSlides - 1].cloneNode(true);
+	 		const firstClone = slides[0].cloneNode(true);
+	 		const lastClone = slides[numSlides - 1].cloneNode(true);
 
-	// 		firstClone.setAttribute('id', 'first-clone');
-	// 		lastClone.setAttribute('id', 'last-clone');
+	 		firstClone.setAttribute('id', 'first-clone');
+	 		lastClone.setAttribute('id', 'last-clone');
 
-	// 		slider.appendChild(firstClone);
-	// 		slider.prepend(lastClone);
+	 		slider.appendChild(firstClone);
+	 		slider.prepend(lastClone);
 
-	// 		const allSlides = document.querySelectorAll('.slider__slide'); //allSlides includes the clones
+	 		const allSlides = document.querySelectorAll('.slider__slide'); //allSlides includes the clones
 
 	// //     // SHOW THE FIRST SLIDE
-	// 		slider.style.transform = `translateX(${-moveX}px)`;
+	 		slider.style.transform = `translateX(${-moveX}px)`;
 
 	// //     // SET COUNTER AND ADD EVENT LISTENERS TO BUTTON;
 	// //     // EACH BUTTON CLICK SHOULD MOVE ALL SLIDES HORIZONTALLY BY MOVEX AMOUNT COUNTER TIMES
-	// 		let counter = 1;
+	 		let counter = 1;
 
-	// 		nextBtn.addEventListener('click', () => {
-	// 			counter++;
-	// 			slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
-	// 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
-	// 			if (counter >= allSlides.length) counter = numSlides;
-	// 			console.log(counter);
-	// 		});
+	 		nextBtn.addEventListener('click', () => {
+	 			counter++;
+	 			slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+	 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+	 			if (counter >= allSlides.length) counter = numSlides;
+	 			console.log(counter);
+	 		});
 
-	// 		prevBtn.addEventListener('click', () => {
-	// 			counter--;
-	// 			slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
-	// 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
-	// 			if (counter < 0) counter = 0
-	// 		});
+	 		prevBtn.addEventListener('click', () => {
+	 			counter--;
+	 			slider.style.transition = 'transform 0.5s ease-in-out, opacity 0.2s';
+	 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+	 			if (counter < 0) counter = 0
+	 		});
 
 	// 		// CHECK FOR FIRSTCLONE AND LASTCLONE AND RESET THE SLIDES ACCORDINGLY
-	// 		slider.addEventListener('transitionend', () => {
-	// 			if (allSlides[counter].id === 'first-clone') {
-	// 				slider.style.transition = 'none';
-	// 				counter = 1;
-	// 				slider.style.transform = `translateX(${-(moveX * counter)}px)`;
-	// 			}
+	 		slider.addEventListener('transitionend', () => {
+	 			if (allSlides[counter].id === 'first-clone') {
+	 				slider.style.transition = 'none';
+	 				counter = 1;
+	 				slider.style.transform = `translateX(${-(moveX * counter)}px)`;			}
 
-	// 			if (allSlides[counter].id === 'last-clone') {
-	// 				slider.style.transition = 'none';
-	// 				counter = numSlides;
-	// 				slider.style.transform = `translateX(${-(moveX * counter)}px)`;
-	// 			}
-	// 		});
+	 			if (allSlides[counter].id === 'last-clone') {
+	 				slider.style.transition = 'none';
+	 				counter = numSlides;
+	 				slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+	 			}
+	 		});
 
 	// //     // ADJUST THE SLIDER POSITION WHEN THE SLIDER/VIEWPORT IS RESIZED
-	// 		window.addEventListener('resize', () => {
-	// 			slideWidth = slides[0].offsetWidth;
-	// 			slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
-	// 			moveX = slideWidth + slideMarginRight;
-	// 			slider.style.transition = 'none';
-	// 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
-	// 		});
+	 		window.addEventListener('resize', () => {
+	 			slideWidth = slides[0].offsetWidth;
+	 			slideMarginRight = parseInt(getComputedStyle(slides[0]).marginRight);
+	 			moveX = slideWidth + slideMarginRight;
+	 			slider.style.transition = 'none';
+	 			slider.style.transform = `translateX(${-(moveX * counter)}px)`;
+	 		});
 			
 
 } );
