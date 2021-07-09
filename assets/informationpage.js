8e0786af51e2db25b2d17b63dbe88225fb121ca8
Léa Buguinet <lea.buguinet@gmail.com>
2021-07-09 16:13:02 +0200

    Information Page Sidebar JS bug fixed + link CSS

--- assets/informationpage.js
+++ assets/informationpage.js
@@ -2,23 +2,30 @@
 // ------------------------------- sidebar for INFORMATION PAGE
 // https://www.bram.us/2020/01/10/smooth-scrolling-sticky-scrollspy-navigation/
 
-window.addEventListener('DOMContentLoaded',() => {
+window.addEventListener('DOMContentLoaded', () => {
 
 	const observer = new IntersectionObserver(entries => {
-		entries.forEach(entry => {
-			const id = entry.target.getAttribute('id');
-			if (entry.intersectionRatio > 0) {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
-			} else {
-				document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
-			}
-		});
-	});
+	  	entries.forEach(entry => {
+		const id = entry.target.getAttribute('id');
 
+		if(document.querySelector(`nav li a[href="#${id}"]`) !== null){
+		
+			if (entry.intersectionRatio > 0) {
+			document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+			} else {
+			document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+			}
+		}
+
+	  	});
+	});
+  
 	// Track all sections that have an `id` applied
 	document.querySelectorAll('section[id]').forEach((section) => {
-		observer.observe(section);
+	  observer.observe(section);
 	});
 	
-});
+  });
+  
+  
 
