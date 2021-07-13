cecef3ca68e0deb2dcaac89dabf0ad373285fbf5
Léa Buguinet <lea.buguinet@gmail.com>
2021-07-13 13:04:11 +0200

    Agency page - before:: elements

--- assets/island.js
+++ assets/island.js
@@ -3,44 +3,49 @@
 
 import Splide from '@splidejs/splide';
 
-// ------ function for photo carousel on ISALND page
-
- 	document.addEventListener( 'DOMContentLoaded', function () {
- 		new Splide( '#image-slider', {
- 			'cover'      : true,
- 			'heightRatio': 0.5,
- 			'height' : '400px',
- 			'width' : '100%',
- 			type   : 'loop',
- 			autoplay : 'true',
- 			type  : 'fade',
- 			rewind: true,
- 		} ).mount();
 
 
-		 // ------ function to drop down information inside tabs
-		 function onClick() {
-			// Récupération de l'élément .tab parent
-			const tab = this.closest('.dropdown');
-			// Récupération de l'élément "frère" suivant (ici .content)
-			const content = tab.nextElementSibling;
+document.addEventListener( 'DOMContentLoaded', function () {
+
+	// ------ function for photo carousel on ISLAND page
+ 	
+	new Splide( '#image-slider', {
+ 		'cover'      : true,
+ 		'heightRatio': 0.5,
+ 		'height' : '400px',
+ 		'width' : '100%',
+ 		type   : 'loop',
+ 		autoplay : 'true',
+ 		type  : 'fade',
+		rewind: true,
+	}).mount();
+
+
+	// ------ function to drop down information inside tabs
+		 
+	function onClick() {
+		// Récupération de l'élément .tab parent
+		const tab = this.closest('.dropdown');
+		// Récupération de l'élément "frère" suivant (ici .content)
+		const content = tab.nextElementSibling;
 		
-			const contentDisplay = document.querySelector('.textcontent.display');
-			if (contentDisplay && content != contentDisplay) {
-				toggle(contentDisplay);
-			}
+		const contentDisplay = document.querySelector('.textcontent.display');
+		if (contentDisplay && content != contentDisplay) {
+			toggle(contentDisplay);
+		}
 		
-			// Ajout/Suppression de la class display
+		// Ajout/Suppression de la class display
 			toggle(content);
-		}
+	};
 		
-		function toggle(element)
-		{
+	function toggle(element){
+
 			element.classList.toggle('display');
-		}
+	};
 		
-		const btns = document.querySelectorAll('.dropdown .btn');
-		btns.forEach(btn => {
-			btn.addEventListener('click', onClick);
-		});
-     })
+	const btns = document.querySelectorAll('.dropdown .btn');
+	btns.forEach(btn => {
+		btn.addEventListener('click', onClick);
+	});
+
+})
