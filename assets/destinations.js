d200ea0fd710e11eb8f1056b76170e6afb78e2c4
Léa Buguinet <lea.buguinet@gmail.com>
2021-07-15 18:12:34 +0200

    Destination page - map links

--- assets/destinations.js
+++ assets/destinations.js
@@ -11,48 +11,10 @@ window.addEventListener('DOMContentLoaded', () => {
             region.addEventListener('mouseover', function(){
                 document.querySelectorAll(className).forEach(regionPiece => {
                     regionPiece.classList.add('map-overlay');
-                })
+                });
 
+                
 
-                let regionName = document.querySelector('.region-name');
-                let regionDetail = document.querySelector('.region-detail');
-
-                switch (className){
-                    case '.map-sumatra':
-                    regionName.textContent = "L'île de Sumatra";
-                    regionDetail.textContent = "";
-                    break;
-
-                    case '.map-sulawesi':
-                    regionName.textContent = "L'île de Sulawesi";
-                    regionDetail.textContent = "";
-                    break;
-
-                    case '.map-kalimantan':
-                    regionName.textContent = "L'île de Kalimantan";
-                    regionDetail.textContent = "";
-                    break;
-
-                    case '.map-bali':
-                    regionName.textContent = "L'île de Bali";
-                    regionDetail.textContent = "";
-                    break;
-
-                    case '.map-java':
-                    regionName.textContent = "L'île de Java";
-                    regionDetail.textContent = "";
-                    break;
-
-                    case '.map-nusatenggara':
-                    regionName.textContent = 'La province de Nusa Tenggara Timur';
-                    regionDetail.textContent = "Elle est composée de la partie orientale des petites îles de la Sonde.";
-                    break;
-
-                    case '.map-papua':
-                    regionName.textContent = 'Papouasie';
-                    regionDetail.textContent = "";
-                    break;
-                }
             })
 
             region.addEventListener('mouseleave', function(){
