9de23d53e0b3e4a5e41f8ea4347a131a5b3c9d8c
Nathalie <nathaliethisse@gmail.com>
2021-09-27 16:15:54 +0200

    News entity creation + crud + display on home and info pages

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,15 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NewsRepository;
 
 class HomeController extends AbstractController
 {
 
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(NewsRepository $newsRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'news' => $newsRepository->findAll(),
         ]);
     }
 }
--- src/Controller/InformationController.php
+++ src/Controller/InformationController.php
@@ -5,16 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\NewsRepository;
 
 class InformationController extends AbstractController
 {
     /**
      * @Route("/information", name="information")
      */
-    public function index(): Response
+    public function index(NewsRepository $newsRepository): Response
     {
         return $this->render('information/index.html.twig', [
-            'controller_name' => 'InformationController',
+            'news' => $newsRepository->findAll(),
         ]);
     }
 }
